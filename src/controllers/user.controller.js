f85a2eb39e5e37ddd03a90717ad600b4b67cebc4
ShriyanshPandey-702 <shriyansh7021639629@gmail.com>
2025-09-24 17:41:59 +0530

    fixed registeration and cloudinary bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -33,12 +33,15 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;  // it will give error of undefined in array when coverImage is not uploaded , another way - if else
 
-    // let coverImageLocalPath;
-    // if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-    //   coverImageLocalPath = req.files.coverImage[0].path;
-    // }
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(404, "Avatar file is required");
